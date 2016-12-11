142c2f15590c5806c3a120177b3e718ffbe2e86d
Nick Balestra <nbalestra@opentable.com>
2016-12-11 12:44:49 +0000

    route validation for single app

--- src/routes/Apps/routes/App/index.js
+++ src/routes/Apps/routes/App/index.js
@@ -1,5 +1,7 @@
 import App from './components/App'
 import Edit from './routes/Edit'
+import store from '../../../../stores'
+
 const route = {
   path: ':appId',
   getChildRoutes(partialNextState, cb) {
@@ -8,6 +10,15 @@ const route = {
 
   getComponents(nextState, cb) {
       cb(null, App)
+  },
+
+  onEnter(nextState, replace, cb){
+    const apps = store.getState().apps.apps
+    if (!apps[nextState.params.appId])
+      replace({
+        pathname: '/apps'
+      })
+    cb()
   }
 }
 
